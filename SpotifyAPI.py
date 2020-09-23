939d4a41d20ad9af04b6494cf29d8286fefd00ff
Tousif Chowdhury <tac5780@rit.edu>
2020-09-23 19:10:26 -0400

    use python3 code

--- SpotifyAPI.py
+++ SpotifyAPI.py
@@ -1,20 +1,13 @@
-import uuid
 import spotipy
-import spotipy.util as util
+from spotipy.oauth2 import SpotifyOAuth
+
 
 class SpotifyAPI:
     def session(self):
         scope = "user-library-read playlist-modify-public user-read-private user-read-email"
-        user = str(uuid.uuid1())
-        
-        token = util.prompt_for_user_token(user, scope)
-        sp = None
-        if token:
-            sp = spotipy.Spotify(auth=token)
-        else:
-            token = util.prompt_for_user_token(user, scope)
-            sp = spotipy.Spotify(auth=token)
-        return sp
+
+        spotify = spotipy.Spotify(auth_manager=SpotifyOAuth(scope=scope))
+        return spotify
 
     def searchForTrack(self,session,token):
         query = "track:"+token
