f867474be892ef408736edcd624a12a70801f3ec
Tousif Chowdhury <tac5780@rit.edu>
2020-09-23 18:27:07 -0400

    push to heroku

--- SongComparer.py
+++ SongComparer.py
@@ -1,24 +1,20 @@
 import re
 from fuzzywuzzy import fuzz
-import logging
 
 
+# TODO: Can add a full ratio check first, if over 90 then do the partial
 class SongComparer:
     def compare(self, songa, songb):
         cleaned_songa = re.sub('[\W_]+', '', songa)
         cleaned_songb = re.sub('[\W_]+', '', songb)
-
         return fuzz.partial_ratio(cleaned_songa.lower(), cleaned_songb.lower())
 
     def song_compare(self, apple_song, spotify_song):
-        # track the percentage on the searches
         if self.compare(apple_song.name, spotify_song.name) > 85:
             if self.compare(apple_song.artist, spotify_song.artist) > 85:
                 if self.compare(apple_song.album, spotify_song.album) > 85:
                     return True
         return False
-
-        # Check 85 percentage match on artist, year, and song name
 
     def find_apple_song_in_spotify_results(self, apple_song, results):
         for spotify_song in results['tracks']['items']:
