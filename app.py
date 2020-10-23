ac2351c0e148505c3e9d7752ca4399b46d4f7201
Tousif Chowdhury <tac5780@rit.edu>
2020-10-22 21:09:07 -0400

    test heroku login

--- app.py
+++ app.py
@@ -17,7 +17,7 @@ song_compare_service = SongComparer.SongComparer()
 @app.route('/get-applemusic-songs')
 def getAppleMusicSongs():
     apple_playlist_id = request.args.get("apple_playlist_id")
-    playlist_name = request.args.get("playlist_name")
+    playlist_name = request.args.get("x`x`")
 
     logging.info("The playlist_id is " + apple_playlist_id)
     apple_songs = []
@@ -61,11 +61,54 @@ def getAppleMusicSongs():
                 logging.error(e.__str__())
 
     return {"FINISH": "FINISH"}
+#
+# @app.route('/')
+# def hello_world():
+#     return 'Hello, World!'
+#
+
+from flask_session import Session
+# app = Flask(__name__)
+app.config['SECRET_KEY'] = os.urandom(64)
+app.config['SESSION_TYPE'] = 'filesystem'
+app.config['SESSION_FILE_DIR'] = './.flask_session/'
+Session(app)
+
+
+caches_folder = './.spotify_caches/'
+if not os.path.exists(caches_folder):
+    os.makedirs(caches_folder)
+
+def session_cache_path():
+    return caches_folder + session.get('uuid')
 
 @app.route('/')
-def hello_world():
-    return 'Hello, World!'
+def index():
+    if not session.get('uuid'):
+        # Step 1. Visitor is unknown, give random ID
+        session['uuid'] = str(uuid.uuid4())
 
+    auth_manager = spotipy.oauth2.SpotifyOAuth(scope='user-read-currently-playing playlist-modify-private',
+                                                cache_path=session_cache_path(),
+                                                show_dialog=True)
+
+    if request.args.get("code"):
+        # Step 3. Being redirected from Spotify auth page
+        auth_manager.get_access_token(request.args.get("code"))
+        return redirect('/')
+
+    if not auth_manager.get_cached_token():
+        # Step 2. Display sign in link when no token
+        auth_url = auth_manager.get_authorize_url()
+        return f'<h2><a href="{auth_url}">Sign in</a></h2>'
+
+    # Step 4. Signed in, display data
+    spotify = spotipy.Spotify(auth_manager=auth_manager)
+    return f'<h2>Hi {spotify.me()["display_name"]}, ' \
+           f'<small><a href="/sign_out">[sign out]<a/></small></h2>' \
+           f'<a href="/playlists">my playlists</a> | ' \
+           f'<a href="/currently_playing">currently playing</a> | ' \
+		   f'<a href="/current_user">me</a>' \
 
 '''
 TODO: Implement Dependency injection later 
